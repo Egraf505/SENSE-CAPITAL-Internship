26a2136444b814e145123c270535fd42d0d50683
EfgraF505 <egor.leskov.03@mail.ru>
2023-03-09 17:37:19 +0300

    Init

---
+++ TicTacToe.WebApi/Program.cs
@@ -0,0 +1,15 @@
+namespace TicTacToe.WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+          
+
+            var app = builder.Build();       
+            app.Run();
+        }
+    }
+}
